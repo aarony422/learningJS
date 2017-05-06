e001c8a82ae4eff3cf8bf6a3af09e7e0f3a49b66
Aaron Young <aaronyoung@Aarons-MBP.attlocal.net>
2017-05-05 19:29:12 -0500

    rendering multiple nodes

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -22,7 +22,31 @@ import React from 'react';
 
 // Another method: stateless function componenet
 // This function can't have state, other methods can
-const App = () => <h1>Hello stateless</h1>
+// const App = () => <h1>Hello stateless</h1>
+
+// render() method can only return a single componenet
+// const App = () => <h1>Hello stateless</h1> <b>Bold</b> // syntax error
+// const App = () => React.createElement() React.createElement() // equivalent to this
+// To solve this, we can wrap our compoenets in a parent componenet
+
+//const App = () => <div><h1>Hello stateless</h1> <b>Bold</b></div>
+
+// Using this method, the first compoenet must be on the same line as the return statement
+// to be safe, use parenthesis!
+class App extends React.Component {
+  // always have a render function
+  // returning the html element using jsx
+  // if you are including a class, we need to use "className" instead of just class
+  // <h1 className="name"> ...
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
 // Export the componenet we created
 export default App
