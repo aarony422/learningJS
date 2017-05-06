1b235bdffa935cc6d7871df70fbbbb2a44d0bab8
Aaron Young <aaronyoung@HollistrsiPhone.attlocal.net>
2017-05-05 19:17:08 -0500

    Hello World with react componenets, 2 ways

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+// Ways to create a react componenet
+// class App extends React.Component {
+//   // always have a render function
+//   // returning the html element using jsx
+//   // if you are including a class, we need to use "className" instead of just class
+//   // <h1 className="name"> ...
+//   render(){
+//     return <h1>Hello World</h1>
+//   }
+// }
 
+// For demonstration purposes
+// All jsx is compiled into javascript
+// class App extends React.Component {
+//   // For custom React componenets, name must start with Capital letter
+//   render(){
+//     return React.createElement('h1', null, 'Hello Eggheads');
+//   }
+// }
+
+// Another method: stateless function componenet
+// This function can't have state, other methods can
+const App = () => <h1>Hello stateless</h1>
+
+// Export the componenet we created
 export default App
