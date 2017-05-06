dc5ff2ea443115c6586d5b16c6b2fa5177c009d8
Aaron Young <aaronyoung@HollistrsiPhone.attlocal.net>
2017-05-05 19:07:29 -0500

    initial commit for first-react-app

---
+++ first-react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; // library that allows us to build react components
+import ReactDOM from 'react-dom'; // library that allows us to work with the DOM
+import App from './App'; //
+
+// render in jsx format to the target 'root'
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
