8188a18a43b1eb4451b71dbad3fc9dfc5e3db3bc
Aaron Young <aarony422@gmail.com>
2017-05-09 22:17:46 -0700

    using react synthetic even system

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -5,6 +5,6 @@ import App from './App'; //
 // render in jsx format to the target 'root'
 // we can pass data into componenets using prop
 ReactDOM.render(
-  <App cat={5} txt="this is the prop value"/>,
+  <App/>,
   document.getElementById('root')
 );
