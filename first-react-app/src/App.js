b421037e4a55b20a154faafbae56fe897086a3a5
Aaron Young <aarony422@gmail.com>
2017-05-12 15:03:35 -0500

    Manage react component state with lifecycle methods, fixed typos component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -18,20 +18,28 @@ class App extends React.Component {
   // fires right before the componenet is mounted to the DOM
   // while render will fire multiple times, componenetWillMount only fires once
   componentWillMount() {
+    // Have access to state and props, but not the DOM representation of the
+    // componenet because it hasn't been mounted yet!
     console.log('componentWillMount');
+    this.setState({m:2});
   }
   // fires after componenet did mount to the DOM
   // Also only fire once
-  componenetDidMount() {
+  componentDidMount() {
+    // access to our componenet in the DOm
     console.log('componenetDidMount');
+    console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update, 500);
   }
   // before componenet leaves DOM
-  componenetWillUnmount() {
+  componentWillUnmount() {
+    // opportunity to clean up any running processes
     console.log('componentWillUnmount');
+    clearInterval(this.inc);
   }
   render() {
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
 }
 
