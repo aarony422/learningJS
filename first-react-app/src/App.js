41fa574c7e717a5944ad4e392980dfca3086a0da
Aaron Young <aarony422@gmail.com>
2017-05-13 19:19:45 -0500

    Understand React.Children

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,44 +1,26 @@
 import React from 'react';
-import './App.css'
-// create in broswer JSX transpiler
+// iterate over children of component
+// if there's only 1 child, we can't map over the children
+// to solve that, React has React.Children
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-                      .transform(code, { presets: ['es2015', 'react']})
-                      .code,
-        err: ''
-      })
-    } catch (err) {
-      this.setState({err: err.message});
-    }
-  }
-
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea
-            onChange={this.update.bind(this)}
-            defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    // let items = React.Children
+    //             .forEach(this.props.children,
+    //               child => console.log(child.props.className));
+    //let items = React.Children.toArray(this.props.children);
+    let items = React.Children.only(this.props.children) // expect only 1 child
+    console.log(items);
+    return null
   }
 }
 
