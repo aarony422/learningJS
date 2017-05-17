24a9825d6813f4f26db8119c4c8cf6ad3f143896
Aaron Young <aarony422@gmail.com>
2017-05-16 19:16:27 -0500

    Redux store methods: getState(), dispatch(), and subscribe()

--- egghead_getting_started_redux/5_counter_reducer.js
+++ egghead_getting_started_redux/5_counter_reducer.js
@@ -1,4 +1,5 @@
 var expect = require('expect')
+var Redux = require('redux')
 
 const counter = (state = 0, action) => {
   // if the reducer receives 'undefined' as the state, it must return what it
@@ -14,29 +15,17 @@ const counter = (state = 0, action) => {
   }
 }
 
-expect(
-  counter(0, { type: 'INCREMENT'})
-).toEqual(1);
+const createStore = Redux.createStore;
+const store = createStore(counter);
 
+// 3 important methods
+// getState(), dispatch(), and subscribe()
+console.log(store.getState());
+store.dispatch({ type: 'INCREMENT' });
+console.log(store.getState());
+// pass in a callback that gets called whenever an action has been dispatched
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-expect(
-  counter(1, { type: 'INCREMENT'})
-).toEqual(2);
-
-expect(
-  counter(2, { type: 'DECREMENT'})
-).toEqual(1);
-
-expect(
-  counter(1, { type: 'DECREMENT'})
-).toEqual(0);
-
-expect(
-  counter(1, {type:'SOMETHING_ELSE'})
-).toEqual(1);
-
-expect(
-  counter(undefined, {})
-).toEqual(0);
-
-console.log('Tests passed!')
+store.dispatch({ type: 'INCREMENT' });
